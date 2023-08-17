173b34b30b20eb6e0a58a600ab85103aa950dd25
Bartek <110336189+Bartek404@users.noreply.github.com>
2023-08-17 23:48:25 +0200

    changed few visual settings, added new function to check holidays

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,42 @@ const currency = document.querySelector('#currency')
 const infoRate = document.querySelector('.info-rate')
 const errorMsg = document.querySelector('.error-msg')
 
+date.valueAsDate = new Date()
+
 const calculate = () => {
 	const APIrequest = `https://api.nbp.pl/api/exchangerates/rates/a/${currency.value}/${date.value}`
 	fetch(APIrequest)
 		.then(res => res.json())
 		.then(data => {
-			console.log(data)
-			console.log(data.rates[0].mid)
 			infoRate.textContent = `Kurs dla ${data.currency} w dniu ${date.value} wynosił średnio: ${data.rates[0].mid} PLN`
 			amountTwo.value = amountOne.value * data.rates[0].mid
 			errorMsg.textContent = ''
 		})
 		.catch(error => {
-			infoRate.textContent = `Prawdopodone święto lub brak danych dla podanego dnia.`
-			errorMsg.textContent = `Komunikat serwera NBP do odczytania w konsoli przeglądarki.`
+			amountTwo.value = 0
+			infoRate.textContent = `Brak danych.`
+			errorMsg.textContent = 'Weekend, lub nieprawidłowa data.'
 			console.error(`Odpowiedź serwara NBP: ${error}`)
+			checkHoliday()
 		})
+}
+
+const checkHoliday = () => {
+	const year = date.value.slice(0, -6)
+	const APIholidayRequest = `https://date.nager.at/api/v3/publicholidays/${year}/PL`
+
+	fetch(APIholidayRequest)
+		.then(res => res.json())
+		.then(data => {
+			data.forEach(element => {
+				if (element.date === date.value) {
+					amountTwo.value = 0
+					infoRate.textContent = 'Brak danych ze względu na święto narodowe:'
+					errorMsg.textContent = `${element.localName}.`
+				}
+			})
+		})
+		.catch(error => console.error(error))
 }
 
 date.addEventListener('change', calculate)
